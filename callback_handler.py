0d78bd6d08e1e8a4c736702fc9f6d97af2ca33fb
Danil <150836172+Chessssman@users.noreply.github.com>
2024-07-12 15:01:50 +0300

    Добавлена обработка "Как пользоваться доставкой?"

--- callback_handler.py
+++ callback_handler.py
@@ -1,11 +1,36 @@
 from aiogram import Router, types
 from aiogram.types import CallbackQuery
 from keyboard import get_start_keyboard  # Импортируйте другие необходимые функции
+from keyboard import get_app_keyboard
 
 # Создаем роутер для callback query
 callback_router = Router()
 
 @callback_router.callback_query()
 async def process_callback(callback: CallbackQuery):
+    if callback.data == "get_product":
+        await callback.message.answer(
+    "Чтобы начать пользоваться бесплатной доставкой, скачайте <b>приложение</b>.\n"
+    "<a href='https://apps.apple.com/us/app/%D0%BE%D0%B7%D0%BE%D0%BD-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD/id407804998'>App Store</a>\n"
+    "<a href='https://play.google.com/store/apps/details?id=ru.ozon.app.android'>Google Play</a>\n"
+    "<a href='https://appgallery.huawei.com/#/app/C100847609'>AppGallery</a>\n"
+    "<a href='https://apps.rustore.ru/app/ru.ozon.app.android'>RuStore</a>\n",
+    parse_mode="HTML",
+    reply_markup= get_app_keyboard()
+)
+
+    elif callback.data == "info":
+        await callback.message.answer(
+            "Подробную информацию о нашем сервисе можно найти <a href='https://example.com/info'>здесь</a>.",
+            parse_mode="HTML"
+        )
+    elif callback.data == "support":
+        await callback.message.answer(
+            "Свяжитесь с нашей <a href='https://t.me/support_channel'>службой поддержки</a>.",
+            parse_mode="HTML"
+        )
+    else:
+        await callback.answer("Неизвестная команда")
     
-    pass
+    # Не забудьте ответить на callback query, чтобы убрать "часики" на кнопке
+    await callback.answer()
--- keyboard.py
+++ keyboard.py
@@ -15,3 +15,13 @@ def get_start_keyboard():
     ]
     keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
     return keyboard
+
+def get_app_keyboard():
+    buttons = [
+        [InlineKeyboardButton(text="App Store", url="https://apps.apple.com/us/app/%D0%BE%D0%B7%D0%BE%D0%BD-%D0%BE%D0%BD%D0%BB%D0%B0%D0%B9%D0%BD-%D0%B8%D0%BD%D1%82%D0%B5%D1%80%D0%BD%D0%B5%D1%82-%D0%BC%D0%B0%D0%B3%D0%B0%D0%B7%D0%B8%D0%BD/id407804998")],
+        [InlineKeyboardButton(text="Google Play", url="https://play.google.com/store/apps/details?id=ru.ozon.app.android")],
+        [InlineKeyboardButton(text="AppGallery", url="https://appgallery.huawei.com/#/app/C100847609")],
+        [InlineKeyboardButton(text="RuStore", url="https://apps.rustore.ru/app/ru.ozon.app.android")]
+    ]
+    keyboard = InlineKeyboardMarkup(inline_keyboard=buttons)
+    return keyboard
