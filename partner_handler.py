b46e05f92875c190e7ddebbf73bca1c34125482c
Danil <150836172+Chessssman@users.noreply.github.com>
2024-12-17 10:05:26 +0300

    Update partner_handler.py

--- partner_handler.py
+++ partner_handler.py
@@ -2,7 +2,6 @@ from aiogram import Router, types
 from aiogram.types import ContentType, InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
-from keyboard import skip_keyboard
 import logging
 
 
